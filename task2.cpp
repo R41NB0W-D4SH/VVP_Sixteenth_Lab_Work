821bdd680b7f75c133b79d65bc5af79a150b5bba
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-23 23:43:23 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -5,42 +5,59 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	int n, i, j;
-	cout << "Введите размерность массива: ";
+	int i, n, n1, j, k, t = 0, el = 0;
+	bool marker = true, ind = false;
+
+	cout << "Введите N размерность массива: ";
 	cin >> n;
 
-	int *arr = new int[n];
-	int *counter = new int[n];
-
+	int* a = new int[n];
 	for (i = 0; i < n; i++)
 	{
-		cout << "Введите элемент массива: ";
-		cin >> arr[i];
-		counter[i] = 0;
+		cout << "a[" << i << "] : ";
+		cin >> a[i];
 	}
-	for (i = 0; i < n; i++)
+
+	int* rewrtd = new int[n];
+	int* buff = new int[n];
+
+	n1 = n;
+
+	for (i = 0; i < n - 1; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = i + 1; j < n; j++)
 		{
-			if (arr[i] == arr[j])
+			if (a[i] == a[j] & a[j] != buff[j])
 			{
-				counter[i]++;
+				if (ind == false) ind = true, el = a[i];
+				else
+				{
+					marker = false;
+					buff[j] = a[j];
+				}
 			}
 		}
-	}
+		if (marker == true & ind == true)
+		{
+			for (k = 0; k < n; k++)
+			{
+				if (a[k] != el)
+				{
+					rewrtd[t] = a[k];
+					t++;
+				}
 
-	for (i = 0; i < n; i++)
-	{
-		if (counter[i] == 2)
-		{ 
-			arr[i] = 0;
+			}
+			t = 0;
+			n1 -= 2;
 		}
+		ind = false;
+		marker = true;
 	}
 
-	for (i = 0; i < n; i++)
-	{
-		cout << "Элемент массива: " << arr[i] << endl;
-	}
+	cout << endl << "Размер массива : " << n1 << endl << endl;
+
+	for (t = 0; t < n1; t++) cout << "b[" << t << "] = " << rewrtd[t] << endl;
 
 	system("pause");
 	return 0;
